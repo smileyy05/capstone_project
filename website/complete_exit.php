b3e183d7bd814fd18481425c11571f9d153c44f7
smileyy05 <nestlepineda8@gmail.com>
2025-12-21 20:37:06 +0800

    Update complete_exit.php

--- website/complete_exit.php
+++ website/complete_exit.php
@@ -8,7 +8,7 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
 // Include database connection
-require_once '../DB/DB_connection.php';
+require_once __DIR__ . '/../DB/DB_connection.php';
 
 // Get JSON input
 $input = json_decode(file_get_contents('php://input'), true);
@@ -146,4 +146,5 @@ try {
         'message' => $e->getMessage()
     ]);
 }
+
 ?>
