290f3fa3ca70249d8d4bad13b1137390fa9cdc58
smileyy05 <nestlepineda8@gmail.com>
2025-12-21 20:30:39 +0800

    Update admin-account-settings.php

--- admin/admin-account-settings.php
+++ admin/admin-account-settings.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['admin'])) {
 }
 
 // Include database connection
-require_once '../DB/DB_connection.php';
+require_once __DIR__ . '/../DB/DB_connection.php';
 
 $success = '';
 $error = '';
@@ -475,4 +475,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     });
   </script>
 </body>
+
 </html>
