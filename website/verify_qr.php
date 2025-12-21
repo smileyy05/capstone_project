73342f9e6292a1cca9ae62eb07ba9a1cf34a61a9
smileyy05 <nestlepineda8@gmail.com>
2025-12-21 20:38:39 +0800

    Update verify_qr.php

--- website/verify_qr.php
+++ website/verify_qr.php
@@ -13,7 +13,7 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
 // Include database connection
-require_once '../DB/DB_connection.php';
+require_once __DIR__ . '/../DB/DB_connection.php';
 
 // Helper function to send JSON response and exit
 function send_json($data) {
@@ -100,4 +100,5 @@ send_json([
         'created_at' => $customer['created_at']
     ]
 ]);
+
 ?>
